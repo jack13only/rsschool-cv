5439963a8cb33b245ed2a50e66da182edc5fde0b
jack13only <jack13only@gmail.com>
2021-09-18 19:36:53 +0300

    feat: add burger menu

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-const foo = {
-    bar() {
-        console.log('a');
-    },
-    name: 'a',
-    age: 2,
+const menu = document.querySelector('.menu-start-item')
+const menuList = document.querySelectorAll('.menu-item')
+
+let interval = 50
+
+function getMenuList() {
+    menu.classList.toggle('open')
+    if (menu.classList.contains('open')) {
+        menu.innerHTML = 'Close'
+    } else menu.innerHTML = 'Menu'
+    menuList.forEach((item, index) => {
+        setTimeout(() => {
+            item.classList.toggle('hidden')
+        }, index * interval)
+    })
 }
+
+
+menu.addEventListener('click', getMenuList)
+
+menuList.forEach(item => {
+    item.addEventListener('click', getMenuList)
+})
